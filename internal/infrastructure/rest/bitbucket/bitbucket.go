d83d68b1664bca05b517857dc3b5ef83cb6f6b78
Daniel Fraile Vergel <daniel.fraile@adidas.com>
2022-05-02 13:29:42 +0200

    Fix: typo in error logic

--- internal/infrastructure/rest/bitbucket/bitbucket.go
+++ internal/infrastructure/rest/bitbucket/bitbucket.go
@@ -1,7 +1,6 @@
 package bitbucket
 
 import (
-	"encoding/json"
 	"net/http"
 
 	bitbucketserver "github.com/go-playground/webhooks/v6/bitbucket-server"
@@ -47,35 +46,38 @@ func (c Handler) WebhookEvent(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	b, _ := json.MarshalIndent(payload, "", "  ")
-	c.apps.LoggerService.WithFields(logger.Fields{
-		"payload": string(b),
-	}).Debug("Bitbucket event received")
+	// b, _ := json.MarshalIndent(payload, "", "  ")
+	// c.apps.LoggerService.WithFields(logger.Fields{
+	// 	"payload": string(b),
+	// }).Debug("Bitbucket event received")
 
-	switch payload.(type) {
+	switch val := payload.(type) {
 	case bitbucketserver.DiagnosticsPingPayload:
-		ping := payload.(bitbucketserver.DiagnosticsPingPayload)
+		// ping := payload.(bitbucketserver.DiagnosticsPingPayload)
 		c.apps.LoggerService.WithFields(logger.Fields{
-			"payload": ping,
+			"type": val,
 		}).Info("DiagnosticsPingEvent")
 		w.WriteHeader(http.StatusOK)
 		return
 	case bitbucketserver.RepositoryReferenceChangedPayload:
 		refChanged := payload.(bitbucketserver.RepositoryReferenceChangedPayload)
 		c.apps.LoggerService.WithFields(logger.Fields{
-			"payload": refChanged,
+			"type": val,
 		}).Info("RepositoryReferenceChangedEvent")
 		for _, r := range refChanged.Changes {
 			c.apps.LoggerService.WithFields(logger.Fields{
 				"branches": r.ReferenceID,
-			}).Info("modified branches")
+			}).Info("RepositoryReferenceChangedEvent")
 			err = c.apps.Commands.ProcessBranch.Handle(commands.ProcessBranchRequest{Branch: r.ReferenceID})
-			if err == nil {
+			if err != nil {
+				c.apps.LoggerService.WithFields(logger.Fields{
+					"err":    err.Error(),
+					"branch": r.ReferenceID,
+				}).Warn("Can not process branch")
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
 		}
 	}
 	w.WriteHeader(http.StatusOK)
-	return
 }
